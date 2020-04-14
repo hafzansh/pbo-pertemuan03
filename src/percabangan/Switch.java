352e88fa2f7541e1a868b10a78d5857b6c24b2e4
hafzansh <hafizansh.m@gmail.com>
2020-04-14 09:50:10 +0800

    test switch menggunakan char

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,24 +2,27 @@ package percabangan;
 
 public class Switch {
     public static void main(String[] args) {
-        int jalanJalan = 3;
-        switch(jalanJalan){
-            case 1:
-                System.out.println("Banjarbaru");
-                break;
-            case 2:
-                System.out.println("Banjarmasin");
-                break;
-            case 3:
-                System.out.println("Pelaihari");
-                break;
-            case 4:
-                System.out.println("Martapura");
-                break;
+        char nilai = 'D';
+        switch(nilai){
+            case 'A':
+                System.out.println("MHS    : \"TerimaKasih Pak\"");
+                System.out.println("Dosen  : \"Selamat ya!!\"");
+            case 'B':
+                System.out.println("MHS    : \"Kenapa B pak?\"");
+                System.out.println("Dosen  : \"!@#$%^&*\"");
+            case 'C':
+                System.out.println("MHS    : \"Turun full kok c pak?\"");
+                System.out.println("Dosen  : \"Ujian dapat negatif itu\"");
+                System.out.println("MHS    : \"kkkkkkk..\"");
             default:
-                System.out.println("Tidak ada tujuan");
+                System.out.println("MHS    : \"Turun full tugas full pak\"");
+                System.out.println("Dosen  : \"is that so??\"");
+                System.out.println("Dosen  : \"checking..\"");
+                System.out.println("Dosen  : \"can't believe his eyes\"");
+                System.out.println("MHS    : \"run in japanese\"");
                 break;
         }
+
     }
 }
 
